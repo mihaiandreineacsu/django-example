db896430c0585aa841b71ae1ce03becfff79b284
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2024-07-18 10:33:29 +0200

    Test Update Post

--- core/tests.py
+++ core/tests.py
@@ -3,7 +3,13 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+from core.models import Post
+
 POST_API_URL = reverse('core:post-list')
+
+def detail_url(post_id):
+    """Create and return a post detail URL."""
+    return reverse('core:post-detail', args=[post_id])
 
 
 # Create your tests here.
@@ -34,3 +40,23 @@ class PostsAPITest(TestCase):
 
         self.assertEqual(response.json().get("author"), user.id)
         self.assertEqual(response.status_code, 201)
+
+    def test_request_update_ok(self):
+        user = User.objects.create(username="Mihai", password="pypassword")
+        user2 = User.objects.create(username="Andrei", password="pypassword")
+        new_post = Post(author=user, title="Some Title", description="Some Description")
+        new_post.save()
+
+        self.client.force_authenticate(user=user)
+
+        payload = {"author": user2.id, "title": "Update Title", "description": "Update Description"}
+        response = self.client.put(detail_url(new_post.id), payload)
+        print(f"response : {response.context}")
+
+        updated_post = Post.objects.get(id=new_post.id)
+
+        self.assertEqual(updated_post.id, new_post.id)
+        self.assertEqual(updated_post.author.id, user.id)
+        self.assertEqual(updated_post.title, "Update Title")
+        self.assertEqual(updated_post.description, "Update Description")
+        self.assertEqual(response.status_code, 200)
--- core/views.py
+++ core/views.py
@@ -6,6 +6,8 @@ from rest_framework import authentication
 
 from core.models import Post
 from core.serializers import PostSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -13,3 +15,7 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [authentication.SessionAuthentication]
+
+    def perform_create(self, serializer):
+        print("my perform_create")
+        serializer.save(author=self.request.user)
