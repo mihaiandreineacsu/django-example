f911298ebd3e73fe782a6748cf85b3e1b02d9674
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2025-02-27 10:38:42 +0100

    core: new model category and update post description to Textfield

--- core/admin.py
+++ core/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from core.models import Post
+from core.models import Category, Post
 
 
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
-    fields = "__all__"
-    list_display = ["author", "title"]
 
 
-admin.site.register(Post)
+@admin.register(Post, Category)
+class CoreAdmin(admin.ModelAdmin):
+    pass
--- core/models.py
+++ core/models.py
@@ -1,31 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.functions import Lower
 
 
 # Create your models here.
 class Post(models.Model):
     """
-    The Core Models for DABooks Posts
+    The Core Model for DABooks Posts
     """
 
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, null=False, blank=False, help_text="The author that created the Post."
     )
     title = models.CharField(
-        max_length=250, null=False, blank=False, help_text="Post title given by author when creating it."
+        max_length=100, null=False, blank=False, help_text="Post title given by author when creating it."
     )
-    description = models.CharField(max_length=1000, null=False, blank=True, default="")
+    description = models.TextField(
+        max_length=1000, null=False, blank=True, default="", help_text="A description what this post is about."
+    )
+
+
+class Category(models.Model):
+    """
+    The Core Model for Categories
+    """
+
+    name = models.CharField(max_length=80, null=False, blank=False, unique=True, help_text="The name of Post category.")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        constraints = [
+            models.UniqueConstraint(
+                Lower("name"),
+                name="unique_lower_name",
+                violation_error_message="This Name already exists (Capital- and Lowercase is ignored).",
+            )
+        ]
 
 
 # TODO Create Categories for Posts -> ManyToMany
 # Fields Constrains
 # Sub Categories -> Example: Main Category "News" then Sb Categories like : "Sport", "Politic" etc...
 # Fields:
-#   - Name:
+#   - Name: unique constrain Ex. Sport == sport
 #   - Color: Django Color Package?
 #   - Description
 #   - Parent Category: Model Category (How to resolve Infinite Loop?)
 #
-
 # Test Feature 1 Bug fix
 # Test Feature 2
