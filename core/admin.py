1d758d23877b63e655be3f1c32a7159cc1f89c70
mihaiandreineacsu <106650061+mihaiandreineacsu@users.noreply.github.com>
2025-03-06 10:33:45 +0100

    update: add new field color to category model

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
 from core.models import Category, Post
+from django.utils.safestring import mark_safe
 
 
-# Register your models here.
+class PostInline(admin.StackedInline):  # Or admin.TabularInline for a table layout
+    model = Post
+    extra = 1  # Number of empty forms to display
 
 
-@admin.register(Post, Category)
-class CoreAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "color_display")
+    search_fields = ("name",)
+    inlines = [PostInline]  # Show Post entries inside the Category admin page
+
+    def color_display(self, obj: Category):
+        return mark_safe(
+            f'<div style="width: 30px; height: 30px; background-color: {obj.color}; border: 1px solid #ccc;"></div>'
+        )
+
+    color_display.short_description = "Color Preview"  # Label for the admin panel
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "title", "category")
+    search_fields = (
+        "title",
+        "description",
+    )
+    list_filter = ("author", "category")
--- core/models.py
+++ core/models.py
@@ -1,6 +1,59 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.functions import Lower
+from colorfield.fields import ColorField
+
+
+def get_or_create_uncategorized():
+    """
+    Get the 'Uncategorized' category, or create it if it doesn't exist.
+    """
+    category, created = Category.objects.get_or_create(
+        name="Uncategorized", defaults={"color": "#000000"}  # Set a default color if creating a new category
+    )
+    return category
+
+
+class Category(models.Model):
+    """
+    The Core Model for Categories
+    """
+
+    COLOR_PALETTE = [
+        (
+            "#FFFFFF",
+            "white",
+        ),
+        (
+            "#000000",
+            "black",
+        ),
+    ]
+
+    name = models.CharField(max_length=80, null=False, blank=False, unique=True, help_text="The name of Post category.")
+    color = ColorField(choices=COLOR_PALETTE)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        constraints = [
+            models.UniqueConstraint(
+                Lower("name"),
+                name="unique_lower_name",
+                violation_error_message="This Name already exists (Capital- and Lowercase is ignored).",
+            )
+        ]
+
+    def delete(self, *args, **kwargs):
+        """
+        Protect the uncategorized Category because is used as Fallback to be set on Posts ForeignKey
+            when deleting other Categories.
+        """
+        if self.name.lower() == "uncategorized":
+            raise ValueError("The 'Uncategorized' category cannot be deleted.")
+        super().delete(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 # Create your models here.
@@ -18,31 +71,20 @@ class Post(models.Model):
     description = models.TextField(
         max_length=1000, null=False, blank=True, default="", help_text="A description what this post is about."
     )
-
-
-class Category(models.Model):
-    """
-    The Core Model for Categories
-    """
-
-    name = models.CharField(max_length=80, null=False, blank=False, unique=True, help_text="The name of Post category.")
-
-    class Meta:
-        verbose_name_plural = "Categories"
-        constraints = [
-            models.UniqueConstraint(
-                Lower("name"),
-                name="unique_lower_name",
-                violation_error_message="This Name already exists (Capital- and Lowercase is ignored).",
-            )
-        ]
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.SET(get_or_create_uncategorized),
+        null=False,
+        blank=False,
+        help_text="Category that matches the Post content.",
+    )
 
 
 # TODO Create Categories for Posts -> ManyToMany
 # Fields Constrains
 # Sub Categories -> Example: Main Category "News" then Sb Categories like : "Sport", "Politic" etc...
 # Fields:
-#   - Name: unique constrain Ex. Sport == sport
+#   - Name: unique constrain Ex. Sport == sport :checked:
 #   - Color: Django Color Package?
 #   - Description
 #   - Parent Category: Model Category (How to resolve Infinite Loop?)
