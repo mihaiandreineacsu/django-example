02edbabbc1d2620f233f7186b0ba5e35a0b04090
mihaiandreineacsu <mihai@developerakademie.com>
2025-06-26 10:45:04 +0200

    chore: example bad formatting

--- demo/settings.py
+++ demo/settings.py
@@ -16,6 +16,8 @@ from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
