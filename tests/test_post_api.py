80a147eefb21baabaee82d7807082b5aa7271823
mihaiandreineacsu <mihai@developerakademie.com>
2026-09-23 12:29:23 +0200

    chore: move the api post tests from api app to root module tests

--- tests/test_post_api.py
+++ tests/test_post_api.py
@@ -1,17 +1,17 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework import status
-from django.urls import reverse
 from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
 from core.models import Post
 
-POST_API_URL = reverse("core:post-list")
+POST_API_URL = reverse("api:post-list")
 
 
 def detail_url(post_id):
     """Create and return a post detail URL."""
-    return reverse("core:post-detail", args=[post_id])
+    return reverse("api:post-detail", args=[post_id])
 
 
 # Create your tests here.
@@ -73,6 +73,26 @@ class PostsAPITest(TestCase):
         # self.assertIsNone(response.json().get("author"))
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_request_create_post_for_other_user(self):
+        user = User.objects.create(username="Mihai", password="pypassword")
+        user2 = User.objects.create(username="Andrei", password="pypassword")
+
+        self.client.force_authenticate(user=user)
+
+        payload = {
+            "author": user2.id,  # ignored by the api and  replaced with the request.user
+            "title": "Some Title",
+            "description": "Some Description",
+        }
+        response = self.client.post(POST_API_URL, payload)
+        with self.assertRaises(Post.DoesNotExist):
+            _ = Post.objects.get(author=user2)
+        self.assertTrue(Post.objects.filter(author=user).exists())
+        print(f"response : {response.context}")
+
+        # self.assertIsNone(response.json().get("author"))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
     def test_request_update_forbidden(self):
         user = User.objects.create(username="Mihai", password="pypassword")
         user2 = User.objects.create(username="Andrei", password="pypassword")
@@ -120,7 +140,7 @@ class PostsAPITest(TestCase):
 
     def test_request_patch_ok(self):
         user = User.objects.create(username="Mihai", password="pypassword")
-        user2 = User.objects.create(username="Andrei", password="pypassword")
+        user2: User = User.objects.create(username="Andrei", password="pypassword")
 
         new_post = Post(author=user2, title="Some Title", description="Some Description")
         new_post.save()
@@ -128,7 +148,7 @@ class PostsAPITest(TestCase):
         self.client.force_authenticate(user=user)
 
         payload = {
-            "author": user2.id,
+            "author": user2.id,  # this should be ignored by the api and replaced with the authenticated user
             "title": "Update Title",
             "description": "Update Description",
         }
@@ -139,4 +159,22 @@ class PostsAPITest(TestCase):
         self.assertEqual(updated_post.author.id, user2.id)
         self.assertEqual(updated_post.title, "Some Title")
         self.assertEqual(updated_post.description, "Some Description")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_request_delete_post_for_other_user_forbidden(self):
+        user = User.objects.create(username="Mihai", password="pypassword")
+        user2: User = User.objects.create(username="Andrei", password="pypassword")
+
+        new_post = Post(author=user2, title="Some Title", description="Some Description")
+        new_post.save()
+
+        self.client.force_authenticate(user=user)
+
+        response = self.client.delete(detail_url(new_post.id))
+
+        to_delete_post = Post.objects.get(id=new_post.id)
+        self.assertEqual(to_delete_post.id, new_post.id)
+        self.assertEqual(to_delete_post.author.id, user2.id)
+        self.assertEqual(to_delete_post.title, "Some Title")
+        self.assertEqual(to_delete_post.description, "Some Description")
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
